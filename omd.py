fix kl grad loss

omd.py
@@ -15,7 +15,7 @@ class omd:
         self.x_t = self.x_t*np.exp(-1*self.eta*loss_grad)
 
         # compute denominator
-        normalizer = np.sum(self.x_t)
+        normalizer = np.sum(np.abs(self.x_t))
         self.x_t = self.x_t/normalizer
 
         return self.x_t
@@ -28,7 +28,7 @@ def loss(x,y, l="l2"):
     if l == "l2":
         return np.linalg.norm(x-y)
     elif l == "l1":
-        return np.abs(np.sum(x-y))
+        return np.sum(np.abs(x-y))
     elif l == "kl":
         # sum over the support of y
         return np.sum(np.where(x != 0,(x-y) * np.log(x / y), 0))
@@ -37,9 +37,9 @@ def grad_loss(x,y, l="l2"):
     if l == "l2":
         return x - y
     if l == "l1":
-        return np.sign(x-y)
+        return np.sum(np.sign(x-y))
     elif l == "kl":
-        return -1* np.divide(x,y)
+        return np.divide(x,y)+np.log(np.divide(x,y))
 
 def obj_func(x):
     return x**2
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     T = 500                     # number of steps
     losses = np.zeros([T,1])    # loss values placeholder
     threshold = 0.0001          # convergence threshold
-    loss_func = "l1"                    # choose loss function
+    loss_func = "kl"                    # choose loss function
 
     p = generate_simplex(dim)
     # p = np.array([0.19775466, 0.16387309, 0.22701363, 0.10678895, 0.30456967])
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         x_t = online_md.step(grad_loss(x_t, p, loss_func))
         loss_t = loss(x_t, p, loss_func)
         losses[t] = loss_t
-
+        
         # check for convergence
         if np.abs(x_t - p).all() < threshold:
             print("solution converged at iter ", t)