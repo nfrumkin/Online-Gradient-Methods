7c9026ad1f8cf9ed57b626a7b5b2009a570fbd83
nfrumkin <nfrumkin@bu.edu>
2020-04-27 10:05:19 -0700

    add kl and l1 losses

--- omd.py
+++ omd.py
@@ -24,11 +24,25 @@ def generate_simplex(dim):
     p = np.random.uniform(0,100, size=[dim])
     return p/np.sum(p)
 
-def l2_loss(x,y):
-    return np.linalg.norm(x-y)
+def loss(x,y, l="l2"):
+    if l == "l2":
+        return np.linalg.norm(x-y)
+    elif l == "l1":
+        return np.abs(np.sum(x-y))
+    elif l == "kl":
+        # sum over the support of y
+        return np.sum(np.where(x != 0,(x-y) * np.log(x / y), 0))
 
-def grad_l2_loss(x,y):
-    return x - y
+def grad_loss(x,y, l="l2"):
+    if l == "l2":
+        return x - y
+    if l == "l1":
+        return np.sign(x-y)
+    elif l == "kl":
+        return -1* np.divide(x,y)
+
+def obj_func(x):
+    return x**2
 
 if __name__ == "__main__":
     dim = 5          # dimension
@@ -36,8 +50,11 @@ if __name__ == "__main__":
     T = 500                     # number of steps
     losses = np.zeros([T,1])    # loss values placeholder
     threshold = 0.0001          # convergence threshold
+    loss_func = "l1"                    # choose loss function
 
     p = generate_simplex(dim)
+    # p = np.array([0.19775466, 0.16387309, 0.22701363, 0.10678895, 0.30456967])
+    # p = p.T
     print("objective simplex: ", p)
 
     online_md = omd(dim, eta)
@@ -46,8 +63,8 @@ if __name__ == "__main__":
     print("initialization: ", x_t)
 
     for t in range(0,T):
-        x_t = online_md.step(grad_l2_loss(x_t, p))
-        loss_t = l2_loss(x_t,p)
+        x_t = online_md.step(grad_loss(x_t, p, loss_func))
+        loss_t = loss(x_t, p, loss_func)
         losses[t] = loss_t
 
         # check for convergence
